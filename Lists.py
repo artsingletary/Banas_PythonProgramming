daa49580984f097fb7e704b1ab49d542c447b250
Art Singletary <artsingletary@yahoo.com>
2023-01-13 13:00:29 -0500

    More fun with lists

--- Lists.py
+++ Lists.py
@@ -16,3 +16,9 @@ print(grocery_list[1:3])
 other_events = ['Wash Car', 'Pick Up Kids', 
                 'Cash']
 
+# Store lists inside of a list
+to_do_list = [other_events, grocery_list]
+print(to_do_list)
+
+# Print the second item in the second list
+print((to_do_list[1][2]))
