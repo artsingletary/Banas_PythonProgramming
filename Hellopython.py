82f1e510c1249494105c0efa4b3aa0872bf659c8
Art Singletary <artsingletary@yahoo.com>
2023-01-13 10:54:04 -0500

    Added arithmetic operator example

---
+++ Hellopython.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import os
+
+print("hello world")
+
+# Comment
+
+'''
+Multiline Comment
+'''
+
+name = "Art"
+print(name)
+
+print ("5 + 2 =", 5+2)
+print ("5 - 2 =", 5-2)
+print ("5 * 2 =", 5*2)
+print ("5 / 2 =", 5/2)
+print ("5 % 2 =", 5%2)
+print ("5 ** 2 =", 5**2)
+print ("5 // 2 =", 5//2)
+
+
+
+
+
+
+
