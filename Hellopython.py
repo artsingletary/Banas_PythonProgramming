e899d19239c862f9ee68e2f7f50b9a2fd50e4cb1
Art Singletary <artsingletary@yahoo.com>
2023-01-13 11:21:59 -0500

    Added print of quote and multi-line quote

--- Hellopython.py
+++ Hellopython.py
@@ -21,9 +21,16 @@ print ("5 % 2 =", 5%2)
 print ("5 ** 2 =", 5**2)
 print ("5 // 2 =", 5//2)
 
+quote = "\"Always remember you are unique\""
 
+multi_line_quote = '''  
+just 
+like everyone 
+else'''
 
+new_string = quote + multi_line_quote
 
+print(new_string)
 
-
+print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
 
