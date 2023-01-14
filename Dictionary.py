777de43708484235b7130b9c3ded61051a2948e5
Art Singletary <artsingletary@yahoo.com>
2023-01-14 09:57:47 -0500

    Added Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+import random
+import sys
+import os
+
+super_villains = {'Fiddler' : 'Isaac Bowin',
+                 'Captain Cold' : 'Leonard Snart',
+                 'Weather Wizard' : 'Mark Mardon',
+                 'Mirror Master' : 'Sam Scudder',
+                 'Pied Piper' : 'Thomas Peterson'
+                 }
+
+# Get the value for the key
+print(super_villains['Captain Cold'])
+
+# Delete a dictionary entry
+del super_villains['Fiddler']
+
+# Change a dictionary value
+super_villains['Pied Piper'] = 'Hartley Rathaway'
+
+# Get the number of key, value pairs we have
+print (len(super_villains))
+
+print(super_villains.get("Fiddler"))
+
+print(super_villains.keys())
+
