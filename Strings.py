2f86e7da3a0db97b240646c893b58c9856379c0c
Art Singletary <artsingletary@yahoo.com>
2023-01-20 14:38:46 -0500

    Updated Strings.py

--- Strings.py
+++ Strings.py
@@ -2,7 +2,7 @@ import random
 import sys
 import os
 
-long_string = "I'll catch you if you fall - The Floor"
+long_string = "     i'll catch you if you fall - The Floor"
 
 # Print first four characters of the string 
 print(long_string[0:4])
@@ -16,45 +16,29 @@ print(long_string[:-5])
 # Print first four characters of one string and concat with another string
 print(long_string[:4] + " be there")
 
-"""
-rand_string = "    this is an important string    "
+print("%c is my %s letter and my number %d number is %.5f" % ('X', 'favorite', 1, .14) ) 
 
-# Strip the black characters to the left
-rand_string = rand_string.lstrip()
+# Capitalize the first letter of a string
+print(long_string.capitalize())
 
-# Strip the black characters to the right
-rand_string = rand_string.rstrip()
+# Return the index value.  The find string is case sensitive so it has to be exact.
+print(long_string.find("Floor"))
 
-# Capitalize the first letter
-print(rand_string.capitalize())
+# Check to see if a string is all characters 
+print('test'.isalpha())
 
-# Print in all caps
-print(rand_string.upper())
+# Check to see if a string is all numbers
+print("12345".isalnum())
 
-# Print in all lower case
-print(rand_string.lower())
+# Get the length of a string
+print(len(long_string))
 
-# Convert a list into a string
-a_list = ["Bunch", "of", "random", "words"]
-print(" ".join(a_list))
+# Replace a word with another word
+print(long_string.replace("Floor","Door"))
 
-# Convert a string into a list
-a_list_2 = rand_string.split()
-for i in a_list_2:
-  print (i)
+# Strip out any white space at the front or back of the string
+print(long_string.strip())
 
-# Count number of occurrences of the word "is"
-print("How many is : ", rand_string.count("is"))
-
-# Replace a string within a string
-print(rand_string.replace("an ", "a kind of "))
-
-# Create an acronym
-orig_string = input("Enter string to convert to acronym: ")
-orig_string = orig_string.upper()
-list_of_words = orig_string.split()
-for word in list_of_words:
-    print(word[0], end="")
-print ()
-"""
-
+# Split a string into a list
+quote_list = long_string.split(" ")
+print(quote_list)
