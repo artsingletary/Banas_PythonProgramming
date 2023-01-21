46b5d0b1433e3bf3706821aaa3feafc6ed40b363
Art Singletary <artsingletary@yahoo.com>
2023-01-21 15:37:56 -0500

    Modified Fileio.py

--- Fileio.py
+++ Fileio.py
@@ -2,8 +2,25 @@ import random
 import sys
 import os
 
+# Create and open a file for writing.  Use "ab+" to read and append to a file.
 test_file = open("test.txt", "wb")
+# Show the mode that was used
 print(test_file.mode)
+# Print the name of the file
 print(test_file.name)
+# Write to the file
 test_file.write(bytes("this is a test\n", 'UTF-8'))
+# Close the file
 test_file.close()
+
+"""
+
+# Open a file for reading and writing 
+test_file = open("test.txt", "r+") 
+text_in_file = test_file.read()
+print (text_in_file)
+test_file.close()
+
+# Remove a file
+os.remove("test.txt")
+"""
